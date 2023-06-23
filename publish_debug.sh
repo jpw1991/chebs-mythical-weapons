103d35e85ae0214d39588fcc3c3b391bd0c2e848
Joshua Paul Woods <jpw1991@gmail.com>
2023-06-23 13:33:30 -0700

    localizations

--- publish_debug.sh
+++ publish_debug.sh
@@ -5,6 +5,7 @@ DLL=$DEBUGDIR/ChebsSwordInTheStone.dll
 LIB=$DEBUGDIR/ChebsValheimLibrary.dll
 BUN=../chebs-necromancy/ChebsNecromancyUnity/Assets/AssetBundles/chebsswordinthestone
 PLUGINS=/home/$USER/.local/share/Steam/steamapps/common/Valheim/BepInEx/plugins
+TRANSLATIONS=Translations
 
 # Check that source files exist and are readable
 if [ ! -f "$DLL" ]; then
@@ -19,6 +20,11 @@ fi
 
 if [ ! -f "$BUN" ]; then
     echo "Error: $BUN does not exist or is not readable."
+    exit 1
+fi
+
+if [ ! -d "$TRANSLATIONS" ]; then
+    echo "Error: $TRANSLATIONS does not exist or is not readable."
     exit 1
 fi
 
@@ -38,3 +44,4 @@ cp -f "$DLL" "$PLUGINS" || { echo "Error: Failed to copy $DLL"; exit 1; }
 cp -f "$LIB" "$PLUGINS" || { echo "Error: Failed to copy $LIB"; exit 1; }
 cp -f "$BUN" "$PLUGINS" || { echo "Error: Failed to copy $BUN"; exit 1; }
 cp -f "$BUN.manifest" "$PLUGINS" || { echo "Error: Failed to copy $BUN.manifest"; exit 1; }
+cp -rf "$TRANSLATIONS" "$PLUGINS" || { echo "Error: Failed to copy $TRANSLATIONS"; exit 1; }
